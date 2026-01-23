8d9af73da8a8873a7c2aca9654b4db458554a43f
OKsusu1411 <hansusu1411@gmail.com>
2026-01-23 18:51:08 +0900

    initial imigration

---
+++ src/lib/controller/PI_controller.py
@@ -0,0 +1,115 @@
+import torch
+
+class PI_Controller():
+    """
+    Low level model-free error feedback PI torque controller
+
+    τ = Kp*e + Ki*∫e
+    """
+
+    def __init__(self, kp, ki, alpha, num_envs: int, num_dof: int, num_leg: int, device: str, dt: float):
+        """
+        Controller initialization  for velocity control
+
+        Args:
+            kp (float or torch.Tensor): Proportional gain. float value or (1, num_dof) size tensor.
+            ki (float or torch.Tensor): Intergral gain. float value or (1, num_dof) size tensor.
+            alpha (float): Low Pass Filter(LPF) gain.  α * new + (1 - α) * old
+            num_envs (int): Number of pararell training environments.
+            num_dof (int): controllable dof per leg.
+            num_leg (int): Number of legs
+            device (str): "cuda:0" or "cpu".
+            dt (float): Simulation time-step.
+        """
+
+        self.device = device
+        self.num_envs = num_envs
+        self.num_dof = num_dof
+        self.num_leg = num_leg
+        self.dt = dt
+        self.alpha = alpha
+        self.old_torque = torch.zeros(self.num_envs, num_dof * num_leg, device=self.device)
+
+        # kp gain
+        if isinstance(kp, float):
+            self.kp = torch.full((num_envs, num_dof), kp, device=device)
+        elif isinstance(kp, torch.Tensor):
+            if kp.shape != (1, num_dof):
+                raise ValueError(f"kp tensor must have shape (1, {num_dof}), but got {kp.shape}")
+            self.kp = kp.to(device).expand(num_envs, -1)        # Expand as num_envs
+        else:
+            raise TypeError("kp must be a float or a torch.Tensor of shape (1, num_dof)")
+
+        # ki gain
+        if isinstance(ki, float):
+            self.ki = torch.full((num_envs, num_dof), ki, device=device)
+        elif isinstance(ki, torch.Tensor):
+            if ki.shape != (1, num_dof):
+                raise ValueError(f"ki tensor must have shape (1, {num_dof}), but got {ki.shape}")
+            self.ki = ki.to(device).expand(num_envs, -1)        # Expand as num_envs
+        else:
+            raise TypeError("ki must be a float or a torch.Tensor of shape (1, num_dof)")
+    
+    def compute_torque(
+        self,
+        joint_vel: torch.Tensor,
+        joint_vel_cmd: torch.Tensor,
+        joint_vel_limits: torch.Tensor,
+        torque_limits: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Compute joint input torque.
+
+        Args:
+            joint_vel (torch.Tensor): Current joint velocity [rad/s].
+            joint_vel_cmd (torch.Tensor): Reference joint velocity(rad/s) [num_env, num_leg, num_joint].
+            joint_limits (torch.Tensor): Joint position limits [num_env, num_joint, 2].
+            torque_limits (torch.Tensor): Joint torque limits [num_env, num_joint].
+
+        Returns:
+            torch.Tensor: Joint torque.
+        """
+
+        # Robot dof
+        leg_dof = self.num_dof                          # wheel joint
+        num_joint = leg_dof * self.num_leg
+
+        # Define joint indices for each leg
+        # Isaac sim's Joint order: ['hip_L_Joint', 'hip_R_Joint', 'thigh_L_Joint', 'thigh_R_Joint', 'knee_L_Joint', 'knee_R_Joint', 'wheel_L_Joint', 'wheel_R_Joint']
+        left_leg_indices = torch.tensor([6], device=self.device, dtype=torch.long)                                      # Hard coded
+        right_leg_indices = torch.tensor([7], device=self.device, dtype=torch.long)                                     # Hard coded
+
+        torque_limits = torque_limits[:, 6:]            # Extract wheel torque limits
+
+        # --- Left Leg slicing ---
+        joint_vel_left = torch.index_select(joint_vel, 1, left_leg_indices)
+        joint_vel_cmd_left = joint_vel_cmd[:, 0].unsqueeze(-1)
+        joint_vel_limits_left = torch.index_select(joint_vel_limits, 1, left_leg_indices)
+
+        # --- Right Leg slicing ---
+        joint_vel_right = torch.index_select(joint_vel, 1, right_leg_indices)
+        joint_vel_cmd_right = joint_vel_cmd[:, 1].unsqueeze(-1)
+        joint_vel_limits_right = torch.index_select(joint_vel_limits, 1, right_leg_indices)
+
+        # Left foot PI control
+        joint_vel_cmd_left = torch.clamp(joint_vel_cmd_left, - joint_vel_limits_left, joint_vel_limits_left)            # Clipping joint position command
+        joint_vel_left_error = joint_vel_cmd_left - joint_vel_left
+        torque_left = self.kp * joint_vel_left_error + self.ki * joint_vel_left_error * self.dt                         # PI feedback
+
+        # Right foot PI control
+        joint_vel_cmd_right = torch.clamp(joint_vel_cmd_right, - joint_vel_limits_right, joint_vel_limits_right)        # Clipping joint position command
+        joint_vel_right_error = joint_vel_cmd_right - joint_vel_right
+        torque_right = self.kp * joint_vel_right_error + self.ki * joint_vel_right_error * self.dt                      # PI feedback
+        
+        # Combine torque inputs
+        torque = torch.zeros(self.num_envs, num_joint, device=self.device)
+        torque = torch.cat((torque_left, torque_right), dim=1)
+        
+        # LPF for torque
+        torque = self.alpha * torque + (1 - self.alpha)* self.old_torque                                                # 0.049 default
+        self.old_torque = torque.clone()
+
+        # Clip torque based on torque_limits
+        torque = torch.clamp(torque, -torque_limits, torque_limits)
+        
+        return torque
---
+++ src/lib/env/GOAT/base/GOAT_base_env_cfg.py
@@ -0,0 +1,180 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import os
+import isaaclab.sim as sim_utils
+
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.actuators import DCMotorCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.utils import configclass
+from isaaclab.terrains import TerrainImporterCfg
+from isaaclab.sim.spawners.materials import RigidBodyMaterialCfg
+from lib.env.env_cfg import EnvCfg
+
+
+# Robot asset paths
+current_dir = os.path.dirname(__file__)
+GOAT_ASSET = {
+    "urdf_path": os.path.join(current_dir, "../assets/GOAT/WF_GOAT/urdf/WF_GOAT.urdf"),
+    "usd_path": os.path.join(current_dir, "../assets/GOAT/WF_GOAT/usd/WF_GOAT.usd"),
+    "usd_place": os.path.join(current_dir, "../assets/GOAT/WF_GOAT/usd/"),
+    "usd_filename": "WF_GOAT.usd"
+}
+
+# URDF to USD conversion
+urdf_cfg: sim_utils.UrdfConverterCfg = sim_utils.UrdfConverterCfg(
+    root_link_name = "base_Link",
+    asset_path = GOAT_ASSET["urdf_path"],
+    usd_dir = GOAT_ASSET["usd_place"],
+    usd_file_name = GOAT_ASSET["usd_filename"],
+    fix_base=False,
+    joint_drive=sim_utils.UrdfConverterCfg.JointDriveCfg(
+        drive_type="force",
+        gains=sim_utils.UrdfConverterCfg.JointDriveCfg.PDGainsCfg(stiffness=0.0, damping=0.0)
+    ),
+)
+urdf_converter = sim_utils.UrdfConverter(cfg = urdf_cfg)
+
+# URDF conversion check
+if urdf_converter.usd_path == GOAT_ASSET["usd_path"]:
+    print("urdf conversion success!")
+else:
+    print("urdf conversion failed!")
+
+GOAT_Cfg: ArticulationCfg = ArticulationCfg(
+    # prim_path="{ENV_REGEX_NS}/Robot",               # Path for Interactivescene's clone_environemnts
+    prim_path="/World/envs/env_.*/Robot",               # Path for DirectRLEnv
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=urdf_converter.usd_path,
+        scale=(1.0, 1.0, 1.0),
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            fix_root_link= False
+        ),
+    ),
+
+    # Link, Joint list in Isaac sim
+    # Link = ['base_Link', 'hip_L_Link', 'hip_R_Link', 'thigh_L_Link', 'thigh_R_Link', 'calf_L_Link', 'calf_R_Link', 'wheel_L_Link', 'wheel_R_Link']
+    # Joint = ['hip_L_Joint', 'hip_R_Joint', 'thigh_L_Joint', 'thigh_R_Joint', 'knee_L_Joint', 'knee_R_Joint', 'wheel_L_Joint', 'wheel_R_Joint']
+    
+    # Initial Joint pos and vel
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.0),
+        joint_pos={
+            "hip_L_Joint": 0.0,
+            "hip_R_Joint": 0.0,
+            "thigh_L_Joint": 0.0,
+            "thigh_R_Joint": 0.0,
+            "knee_L_Joint": 0.0,
+            "knee_R_Joint": 0.0,
+            "wheel_L_Joint": 0.0,
+            "wheel_R_Joint": 0.0,
+            },
+        ),
+
+    # Actuators cfg
+    actuators={
+        "hip": DCMotorCfg(
+            joint_names_expr=["hip_.*",],
+            effort_limit=4.5,
+            saturation_effort=4.5,
+            velocity_limit=15.0,
+            stiffness=0.0,                          # Internal PD controller not used
+            damping=0.0,                            # Internal PD controller not used
+            friction=0.0033,                        # Static friction coefficient
+            dynamic_friction=0,                     # Dynamic friction coefficient 
+            viscous_friction=0,                     # Viscous friction coefficient
+        ),
+
+        "thigh": DCMotorCfg(
+            joint_names_expr=["thigh_.*",],
+            effort_limit=4.5,
+            saturation_effort=4.5,
+            velocity_limit=15.0,
+            stiffness=0.0,
+            damping=0.32,
+            friction=0.0033,
+            dynamic_friction=0,
+            viscous_friction=0,
+        ),
+
+        "knee": DCMotorCfg(
+            joint_names_expr=["knee_.*",],
+            effort_limit=4.5,
+            saturation_effort=4.5,
+            velocity_limit=15.0,
+            stiffness=0.0,
+            damping=0.0,
+            friction=0.0235,
+            dynamic_friction=4.432008e-01,
+            viscous_friction=2.993308e-01,
+        ),
+        
+        "wheel": DCMotorCfg(
+            joint_names_expr=["wheel_.*"],
+            effort_limit=2.5,
+            saturation_effort=2.5,
+            velocity_limit=15.0,
+            stiffness=0.0,
+            damping=0.0,
+            friction=None,
+            dynamic_friction=4.432008e-01,
+            viscous_friction=2.993308e-01,
+        )
+    }
+)
+
+@configclass
+class GOATBaseEnvCfg(EnvCfg):
+    # Env
+    episode_length_s: int = 10       # Episode length in seconds
+    sim_dt: float = 0.01             # Simulation(low-level controller) frequency
+    decimation: int = 3              # Policy frequency = sim_freq / decimation
+    action_space: int = 0            # Dimension of action space vector
+    observation_space: int = 0       # Dimension of observation space vector
+    state_space: int = 0             # Dimension of state space vector for privileged RL
+
+    # Terrain
+    terrain = TerrainImporterCfg(
+        prim_path="/World/ground",
+        terrain_type="plane",        # Should apply terrain generator later
+        env_spacing=3.0,
+        physics_material=sim_utils.RigidBodyMaterialCfg(
+            static_friction=1.0,
+            dynamic_friction=0.8,
+            restitution=0.0
+        ),
+        debug_vis=False
+    )
+
+    physics_material: RigidBodyMaterialCfg = RigidBodyMaterialCfg(
+        static_friction=1.0,
+        dynamic_friction=1.0,
+        restitution=0.0,
+    )
+
+    # Light
+    dome_light_cfg = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
+
+    # Scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=1, env_spacing=3.0, replicate_physics=True)
+
+    # GOAT cfg
+    GOAT_cfg: ArticulationCfg = GOAT_Cfg
