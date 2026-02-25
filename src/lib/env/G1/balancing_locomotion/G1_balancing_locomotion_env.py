bf904eb4446c979519ada80a8a368245f28785a2
Diligent_Yoon <grape43146@naver.com>
2026-02-25 19:20:27 +0900

    ang vel

--- src/lib/env/G1/balancing_locomotion/G1_balancing_locomotion_env.py
+++ src/lib/env/G1/balancing_locomotion/G1_balancing_locomotion_env.py
@@ -420,6 +420,7 @@ class G1BalancingLocomotionEnv(G1BaseEnv):
                              self.cfg.w_track_ang_vel * ang_vel_rewards + \
                              self.cfg.w_track_heading * heading_rewards + \
                              self.cfg.w_track_height  * height_rewards + \
+                             self.cfg.w_ang_vel_xy    * ang_vel_xy_penalty + \
                              self.cfg.w_flat          * flat_penalty + \
                              self.cfg.w_termination   * terminate_penalty
 
