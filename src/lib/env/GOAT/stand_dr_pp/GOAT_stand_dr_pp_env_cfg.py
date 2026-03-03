a278fe2a4e1f5d9c5d782f5b80f4cd6f3323f1a2
Home <grape4314@cau.ac.kr>
2026-03-03 23:02:10 +0900

    .

--- src/lib/env/GOAT/stand_dr_pp/GOAT_stand_dr_pp_env_cfg.py
+++ src/lib/env/GOAT/stand_dr_pp/GOAT_stand_dr_pp_env_cfg.py
@@ -141,17 +141,17 @@ class GOATStandDRPPEnvCfg(GOATBaseEnvCfg):
 
     r_upright_weight = 3.0
     r_height_weight = 2.0
-    r_alive_weight = 1.0
+    r_alive_weight = 2.0
 
     p_lin_vel_weight = 0.1
     p_ang_vel_weight = 0.1
     p_joint_limit_weight = 10.0
     p_joint_deviation = 2.0
-    p_all_torque_limit_weight = 0.05
-    p_all_torque_weight = 0.005
+    p_all_torque_limit_weight = 0.01
+    p_all_torque_weight = 0.001
     p_joint_velocity_weight = 0.01
-    p_action_rate_weight = 0.5
-    p_terminated_weight = 100.0
+    p_action_rate_weight = 0.1
+    p_terminated_weight = 200.0
     
 
     ## ==================== Plot variables ==================== ##
