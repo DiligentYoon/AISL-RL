14aee7f1fd122c84365bd97b7a641085d0e1a7e9
Diligent_Yoon <grape43146@naver.com>
2026-03-03 19:59:27 +0900

    .

--- src/lib/env/GOAT/stand_dr_pp/GOAT_stand_dr_pp_env_cfg.py
+++ src/lib/env/GOAT/stand_dr_pp/GOAT_stand_dr_pp_env_cfg.py
@@ -146,7 +146,7 @@ class GOATStandDRPPEnvCfg(GOATBaseEnvCfg):
     p_lin_vel_weight = 0.1
     p_ang_vel_weight = 0.1
     p_joint_limit_weight = 10.0
-    p_joint_deviation = 5.0
+    p_joint_deviation = 1.0
     p_all_torque_limit_weight = 0.05
     p_all_torque_weight = 0.005
     p_joint_velocity_weight = 0.01
